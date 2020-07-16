a132c57441eea0c76ace520217a4e2a0ca27e9a8
olyakalun1998@gmail.com <olyakalun1998@gmail.com>
2020-07-16 15:19:00 +0300

    Customized api hamhab for React UI

---
+++ humhub-1.4.2/index.php
@@ -0,0 +1,34 @@
+<?php
+
+//die('fdsfdsfds');
+
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require(__DIR__ . '/protected/vendor/autoload.php');
+require(__DIR__ . '/protected/vendor/yiisoft/yii2/Yii.php');
+
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/protected/humhub/config/common.php'),
+    require(__DIR__ . '/protected/humhub/config/web.php'),
+    (is_readable(__DIR__ . '/protected/config/dynamic.php')) ? require(__DIR__ . '/protected/config/dynamic.php') : [],
+    require(__DIR__ . '/protected/config/common.php'),
+    require(__DIR__ . '/protected/config/web.php')
+);
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+
+
+(new humhub\components\Application($config))->run();
---
+++ humhub-1.4.2/protected/config/dynamic.php
@@ -0,0 +1,54 @@
+<?php return array (
+  'components' => 
+  array (
+    'db' => 
+    array (
+      'class' => 'yii\\db\\Connection',
+      'dsn' => 'mysql:host=localhost;dbname=humhub',
+      'username' => 'root',
+      'password' => '',
+    ),
+    'user' => 
+    array (
+    ),
+    'mailer' => 
+    array (
+      'transport' => 
+      array (
+        'class' => 'Swift_MailTransport',
+      ),
+    ),
+    'cache' => 
+    array (
+      'class' => 'yii\\caching\\FileCache',
+      'keyPrefix' => 'humhub',
+    ),
+    'formatter' => 
+    array (
+      'defaultTimeZone' => 'UTC',
+    ),
+    'formatterApp' => 
+    array (
+      'defaultTimeZone' => 'UTC',
+      'timeZone' => 'UTC',
+    ),
+  ),
+  'params' => 
+  array (
+    'installer' => 
+    array (
+      'db' => 
+      array (
+        'installer_hostname' => 'localhost',
+        'installer_database' => 'humhub',
+      ),
+    ),
+    'config_created_at' => 1584456337,
+    'horImageScrollOnMobile' => '1',
+    'databaseInstalled' => true,
+    'installed' => true,
+  ),
+  'name' => 'HumHub',
+  'language' => 'en-US',
+  'timeZone' => 'UTC',
+); ?>
